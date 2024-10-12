0491d4e3801d39e0955f6665719979e2478d3acd
Juan David <juandavid918@gmail.com>
2024-10-12 15:44:46 -0500

    Solución a reemplazos de CSS

--- container/src/App.js
+++ container/src/App.js
@@ -1,17 +1,24 @@
 import { BrowserRouter } from "react-router-dom";
+import { createGenerateClassName, StylesProvider } from "@material-ui/core/styles";
 import React from "react";
 
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: "co"
+});
+
 export default () => {
     return (
-        <BrowserRouter>
-            <div>
-                <Header />
+        <StylesProvider generateClassName={ generateClassName }>
+            <BrowserRouter>
+                <div>
+                    <Header />
 
-                <MarketingApp />
-            </div>
-        </BrowserRouter>
+                    <MarketingApp />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     );
 }
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
+import { createGenerateClassName, StylesProvider } from "@material-ui/core/styles";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import React from "react";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: "ma"
+});
+
 export default () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={ generateClassName }>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path="/pricing" component={ Pricing } />
