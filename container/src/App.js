cc7d99931fcddbe93e64c463f27ab1ae6dc1bf1d
Juan David <juandavid918@gmail.com>
2024-10-19 12:07:05 -0500

    Comunicación de autenticación entre los microfrontends

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { createGenerateClassName, StylesProvider } from "@material-ui/core/styles";
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useState } from "react";
 
 import Header from "./components/Header";
 import Progress from "./components/Progress";
@@ -13,15 +13,19 @@ const generateClassName = createGenerateClassName({
 });
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={ generateClassName }>
                 <div>
-                    <Header />
+                    <Header onSignOut={ () => setIsSignedIn(false) } isSignedIn={ isSignedIn } />
 
                     <Suspense fallback={ <Progress /> }>
                         <Switch>
-                            <Route path="/auth" component={ AuthLazy } />
+                            <Route path="/auth">
+                                <AuthLazy onSignIn={ () => setIsSignedIn(true) } />
+                            </Route>
 
                             <Route path="/" component={ MarketingLazy } />
                         </Switch>
